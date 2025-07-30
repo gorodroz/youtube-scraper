bf99ef3813b405a0502d74f923e6181314205302
gorodroz <izahorulko860@gmail.com>
2025-07-30 18:18:36 +0300

    add dictionary for navigation on quality

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-url = input("Paste Link:")
+quality_vid = {
+    "360p mp4": "18",
+    "2160p no audio": "701",
+    "1440p no audio": "700",
+    "1080p no audio": "137",
+    "720p no audio": "136",
+    "480p no audio": "135",
+    "360p no audio": "134",
+    "240p no audio": "133",
+    "144p no audio": "160"
+}
+
+quality_aud = {
+    "160kbps opus": "251",
+    "128kbps m4a": "140",
+    "50kbps m4a": "249",
+    "48kbps m4a": "139"
+}
+
+url = input("Paste Youtube Link:")
 
 video = YouTube(
     url=url,
@@ -9,20 +28,30 @@ video = YouTube(
     use_oauth=False,
     allow_oauth_cache=False
 )
+minsmedia = int(video.length/60)
 
-print('Title:', video.title)
-print("Which format you want to download? Audio or Video")
+print('Title:', video.title, minsmedia, "mins")
+print("Which format you want to download? \"Audio\" or \"Video\"")
 ytaudvid = input("")
 if ytaudvid == "Audio":
     audio = video.streams.filter(only_audio=True)
-    print(video.streams.filter(only_audio=True))
-    stream = video.streams.get_by_itag(input("Write itag for download:"))
+    userqlt = input(f"Choice quality {list(quality_aud.keys())}: ")
+    getqlt = quality_aud.get(userqlt)
+    if not getqlt:
+        print("Error. You enter unavaible quality. Exit")
+        exit()
+    stream = video.streams.get_by_itag(getqlt)
+
 if ytaudvid == "Video":
     ovideo = video.streams.filter(file_extension='mp4')
-    print(video.streams.filter(file_extension='mp4'))
-    stream = video.streams.get_by_itag(input("Write itag for download:"))
+    userqlt = input(f"Choice quality {list(quality_vid.keys())}: ")
+    getqlt = quality_vid.get(userqlt)
+    if not getqlt:
+        print("Error. You enter unavaible quality. Exit")
+        exit()
+    stream = video.streams.get_by_itag(getqlt)
 
-print("Do you want to change path for download? Write Yes or No.")
+print("Do you want to change path for download? Write \"Yes\" or \"No\".")
 pathdownload = input("")
 if pathdownload == "Yes":
     stream.download(output_path=input("Enter path:"))
@@ -30,4 +59,3 @@ elif pathdownload == "No":
     stream.download(output_path="X:\youtube-scraper\downloads")
 
 print("Successful!")
-
